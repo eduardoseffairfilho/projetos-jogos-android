8a0a61dcc4425bf6e3c1d9cf59c21e2d119d2601
eduardoseffairfilho@gmail.com <eduardoseffairfilho@gmail.com@531118cb-8a9f-7f0d-aa31-307e80916440>
2014-03-11 13:33:12 +0000

    Desenhando o inimigo e o player.

--- Impossible/src/br/com/casadocodigo/impossible/Game.java
+++ Impossible/src/br/com/casadocodigo/impossible/Game.java
@@ -1,22 +1,49 @@
 package br.com.casadocodigo.impossible;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class Game extends Activity {
+public class Game extends Activity implements OnTouchListener {
+	
+	Impossible view;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_game);
+		
+		// Coloca em fullscreen o jogo
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		// Lógica do Jogo
+		view = new Impossible(this);
+		
+		// Touch Listener.
+		view.setOnTouchListener(this);
+		
+		// Configura view
+		setContentView(view);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_game, menu);
-		return true;
+	protected void onResume() {
+		super.onResume();
+		
+		Toast.makeText(Game.this, "Carregando o jogo...", Toast.LENGTH_LONG).show();
+		
+		view.resume();
 	}
 
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		view.moveDown(10);
+		return true;
+	}
+	
 }
